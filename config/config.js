db37116ee5b1c21beae90e73da42b3e3d6d5fa6c
Evgen-Polyanskii <M4x1mOrlov@yandex.ru>
2022-02-24 19:10:03 +0300

    Изменение конфига для подключения postgres на heroku

--- config/config.js
+++ config/config.js
@@ -19,10 +19,8 @@ module.exports = {
     dialect: 'sqlite',
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DATABASE_URL,
-    dialect: 'postgres',
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
   },
 };
