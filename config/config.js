3df9822be3b77789c5311e97d1c502ed1824af5f
Evgen-Polyanskii <M4x1mOrlov@yandex.ru>
2022-03-03 23:47:21 +0300

    Исправление ошибки

--- config/config.js
+++ config/config.js
@@ -21,7 +21,7 @@ module.exports = {
   production: {
     // username: process.env.DB_USERNAME,
     // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
+    database: process.env.DB_NAME,
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
