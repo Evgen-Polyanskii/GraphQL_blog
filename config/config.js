bade8797a69c11f28103ead82a0f8f5592b0cc0b
Evgen-Polyanskii <M4x1mOrlov@yandex.ru>
2022-03-04 21:38:29 +0300

    Исправление ошибки

--- config/config.js
+++ config/config.js
@@ -23,7 +23,7 @@ module.exports = {
     // password: process.env.DB_PASSWORD,
     // database: process.env.DB_NAME,
     // host: process.env.DB_HOST,
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable: "DATABASE_URL",
     useNullAsDefault: true,
     // port: process.env.DB_PORT || 5432,
     ssl: {
