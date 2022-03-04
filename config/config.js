4d81cc23fc6d83e702bee030e4911abf936c9361
Evgen-Polyanskii <M4x1mOrlov@yandex.ru>
2022-03-04 10:45:12 +0300

    Исправление ошибки

--- config/config.js
+++ config/config.js
@@ -25,7 +25,7 @@ module.exports = {
     // password: process.env.DB_PASSWORD,
     // database: process.env.DB_NAME,
     // host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
+    // port: process.env.DB_PORT || 5432,
     // ssl: {
     //   rejectUnauthorized: false,
     //   require: true,
