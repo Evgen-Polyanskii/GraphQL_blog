238d02caf38b81ba4c5882dfc8fe7d7cc1535562
Evgen-Polyanskii <M4x1mOrlov@yandex.ru>
2022-03-04 10:52:57 +0300

    Исправление ошибки

--- config/config.js
+++ config/config.js
@@ -19,17 +19,8 @@ module.exports = {
     dialect: 'sqlite',
   },
   production: {
-    username: process.env.DB_USERNAME,
-    connectionString: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL,
     useNullAsDefault: true,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT || 5432,
-    // ssl: {
-    //   rejectUnauthorized: false,
-    //   require: true,
-    // },
     dialect: 'postgres',
   },
 };
