b3dde113ab27f3717b68645e940c4455fc69ea31
Evgen-Polyanskii <M4x1mOrlov@yandex.ru>
2022-03-09 17:53:36 +0300

    Исправление ошибки heroku

--- server/bin/server.js
+++ server/bin/server.js
@@ -1,16 +1,7 @@
 #! /usr/bin/env node
 
-const path = require('path');
-const dotenv = require('dotenv');
 const { createServer } = require('../index.js');
 const db = require('../../db/models');
-
-const pathToEnv = path.resolve(__dirname, '../.env');
-
-dotenv.config({ path: pathToEnv });
-
-const port = process.env.PORT || 4000;
-const host = process.env.HOST || 'localhost';
 
 const { app, httpServer, server } = createServer();
 
@@ -18,11 +9,11 @@ db.sequelize.authenticate()
   .then(() => db.sequelize.sync({ alter: true, logging: false }))
   .then(() => server.start())
   .then(() => server.applyMiddleware({ app, path: '/graphql' }))
-  .then(() => httpServer.listen(port, host, (err) => {
+  .then(() => httpServer.listen(app.get('port'),(err) => {
     if (err) {
       console.log('Error', err);
     }
-    console.log(`Apollo Server on http://${host}:${port}/graphql`);
+    console.log(`Apollo Server on http://localhost:${app.get('port')}/graphql`);
   }))
   .catch((err) => {
     console.log(err);
--- server/index.js
+++ server/index.js
@@ -3,19 +3,27 @@ const { ApolloServer } = require('apollo-server-express');
 const { graphqlUploadExpress } = require('graphql-upload');
 const { typeDefs: scalarTypeDefs } = require('graphql-scalars');
 const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const path = require('path');
+const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cors = require('cors');
 const http = require('http');
 const db = require('../db/models');
 const { getUser } = require('./lib/secure.js');
-const getRoutes = require('./routes/users.js');
 
+const getRoutes = require('./routes/users.js');
 const PostAPI = require('./datasources/PostAPI.js');
 const UserAPI = require('./datasources/UserAPI.js');
-const CommentAPI = require('./datasources/CommentAPI.js');
 
+const CommentAPI = require('./datasources/CommentAPI.js');
 const typeDefs = require('./schema/index.js');
 const resolvers = require('./resolvers/index.js');
+
+const pathToEnv = path.resolve(__dirname, '../.env');
+
+dotenv.config({ path: pathToEnv });
+
+const port = process.env.PORT || 4000;
 
 const logger = morgan('combined');
 
@@ -29,6 +37,7 @@ const context = (req) => getUser(req);
 
 const createServer = () => {
   const app = express();
+  app.set('port', (port));
   app.use(cors());
   app.use(logger);
   app.use(graphqlUploadExpress());
